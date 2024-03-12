3259d784727d1f5e38e92dee8c5ac44c7d876dd0
Your Name <you@example.com>
2024-03-12 22:12:47 +0000

    enhancement: added password hashing to user generation

---
+++ src/users/controllers.js
@@ -0,0 +1,38 @@
+const User = require("./model");
+
+const bcrypt = require("bcrypt");
+
+const saltRounds = 10;
+
+const signupUser = async (req, res) => {
+  try {
+    const user = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    const plainTextPassword = req.body.password;
+    const hashPass = async () => {
+      let hash = await bcrypt.hash(plainTextPassword, saltRounds);
+      console.log(hash);
+    };
+    hashPass();
+    res.status(201).json({ message: "user added", user: user });
+  } catch (error) {
+    res.status(500).json({ message: error.message, error: error });
+  }
+};
+
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.status(200).json({ message: "Here are all the users", users: users });
+  } catch (error) {
+    res.status(500).json({ message: error.message, error: error });
+  }
+};
+
+module.exports = {
+  signupUser: signupUser,
+  getAllUsers,
+};
